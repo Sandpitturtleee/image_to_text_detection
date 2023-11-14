c2d3193fd2bcb10655042278b1fcfed86df2fb4a
Sandpitturtleee <karolmazur@onet.eu>
2023-11-14 10:22:29 +0100

    Black isort reformatting, changing reading images from a path directory

--- scr/text_recognition/main.py
+++
@@ -1,8 +0,0 @@
-from demo import recognize_text
-
-from definitions import ROOT_DIR
-
-RAW_IMAGES_DIR = ROOT_DIR + "/database/raw_images/"
-
-if __name__ == "__main__":
-    print(recognize_text(img_path=RAW_IMAGES_DIR + "demo.png"))
