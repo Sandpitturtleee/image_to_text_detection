f703c6003c917694cdc3fa559fd54c0e745932cd
Sandpitturtleee <karolmazur@onet.eu>
2023-11-15 19:21:27 +0100

    Adding 3 different models for text recognition

--- scr/text_recognition/main.py
+++ scr/text_recognition/main.py
@@ -1,10 +1,12 @@
 import os
 from pathlib import Path
 
+from matplotlib import pyplot as plt
+
 from demo import recognize_text
 
 from definitions import CUT_IMAGES_DIR, RESULTS_TXT_DIR
-
+import easyocr
 
 def save_to_txt(result: str, file: str):
     file_name = Path(file).stem
@@ -19,6 +21,27 @@ def detect_and_save_to_file():
         result = recognize_text(img_path=CUT_IMAGES_DIR + file)
         save_to_txt(result=result, file=file)
 
+import keras_ocr
+
 
 if __name__ == "__main__":
-    detect_and_save_to_file()
+    print()
+    #detect_and_save_to_file()
+
+    # pipeline = keras_ocr.pipeline.Pipeline()
+    # images = [
+    #     keras_ocr.tools.read(img) for img in [
+    #         '/Users/sold/Desktop/Python/Projects/image_to_text_detection/database/cut_images/demo1.png',
+    #     ]
+    # ]
+    # # Predictions is a list of (string, box) tuples.
+    # predictions = pipeline.recognize(images)
+    #
+    # predicted_image_1 = predictions[0]
+    # for text, box in predicted_image_1:
+    #     print(text)
+
+    # reader = easyocr.Reader(['ch_sim', 'en'])  # this needs to run only once to load the model into memory
+    # result = reader.readtext('/Users/sold/Desktop/Python/Projects/image_to_text_detection/database/cut_images/demo1.png')
+    # for item in result:
+    #     print(item[1])
