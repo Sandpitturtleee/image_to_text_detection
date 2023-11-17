d1a92811eb0c5aea90969108289e6caaeed65c2a
Sandpitturtleee <karolmazur@onet.eu>
2023-11-17 09:36:47 +0100

    Adding training

--- definitions.py
+++ definitions.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CUT_IMAGES_DIR = ROOT_DIR + "/database/cut_images/"
-RAW_IMAGES_DIR = ROOT_DIR + "/database/raw_images/"
-RESULTS_TXT_DIR = ROOT_DIR + "/database/results_txt/"
+CUT_IMAGES_DIR = ROOT_DIR + "/database/detect/cut_images/"
+RAW_IMAGES_DIR = ROOT_DIR + "/database/detect/raw_images/"
+RESULTS_TXT_DIR = ROOT_DIR + "/database/detect/results_txt/"
+TRAIN_DATASET_PATH = ROOT_DIR + "/database/train/coco128.yaml"
--- scr/image_cropping/main.py
+++ scr/image_cropping/main.py
@@ -1,4 +1,8 @@
-from scr.image_cropping.functions import object_recognition_and_cropping
+from definitions import TRAIN_DATASET_PATH
+from scr.image_cropping.functions import object_recognition_and_cropping, train_model
 
 if __name__ == "__main__":
-    object_recognition_and_cropping()
+    print("OK")
+    #object_recognition_and_cropping()
+    train_model(dataset_path=TRAIN_DATASET_PATH)
+
