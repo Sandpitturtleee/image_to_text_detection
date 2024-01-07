b90dac0cdc7e03179b2325a8ca6a880b5021797f
Sandpitturtleee <karolmazur@onet.eu>
2024-01-07 22:16:12 +0100

    Adding clearing folders black isort formatting

--- scr/image_cropping/functions.py
+++ scr/image_cropping/functions.py
@@ -1,10 +1,21 @@
 import collections
 import os
+import shutil
 from pathlib import Path
 
 import cv2
-
+from pdf2image import convert_from_path
 from ultralytics import YOLO
+
+from definitions import (ARTICLES_CROPPED_DIR, ARTICLES_DIR, NEWSPAPERS_DIR,
+                         PAGES_DIR, POPPLER_PATH, RESULTS_DIR)
+
+# pip install pdf2image
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# brew install poppler
+# brew --prefix poppler
+# pop_path = "/opt/homebrew/opt/poppler/bin"
+
 
 """
     A file containing functions that are used to detect objects on images an crop them into new images
@@ -13,39 +24,50 @@ from ultralytics import YOLO
 
 def detect_and_crop_images(model_name: str, input_folder: str, output_folder: str):
     """
-        Detect classes images and crop them to create new img files.
+    Detect classes images and crop them to create new img files.
 
-        Parameters:
-        :param model_name: Model to use for object detection
-        :type model_name: str
-        :param input_folder: Input folder for images to recognise classes
-        :type input_folder: str
-        :param output_folder: Output folder for saving cropped images
-        :type output_folder: str
+    Parameters:
+    :param model_name: Model to use for object detection
+    :type model_name: str
+    :param input_folder: Input folder for images to recognise classes
+    :type input_folder: str
+    :param output_folder: Output folder for saving cropped images
+    :type output_folder: str
     """
     model = YOLO(model_name)
     for file in os.listdir(input_folder):
         results = model(input_folder + file, device="mps")
         detected_classes = get_cropped_images_names(model=model, results=results)
-        crop_image(file=file, results=results, detected_classes=detected_classes, input_folder=input_folder,
-                   output_folder=output_folder)
+        crop_image(
+            file=file,
+            results=results,
+            detected_classes=detected_classes,
+            input_folder=input_folder,
+            output_folder=output_folder,
+        )
 
 
-def crop_image(file: str, results: list, detected_classes: list, input_folder: str, output_folder: str):
+def crop_image(
+    file: str,
+    results: list,
+    detected_classes: list,
+    input_folder: str,
+    output_folder: str,
+):
     """
-        Crops and saves an images with regard to detected bounding boxes.
+    Crops and saves an images with regard to detected bounding boxes.
 
-        Parameters:
-        :param file: Input img file on which cropping is performed
-        :type file: str
-        :param results: Detected bounding boxes
-        :type results: list
-        :param detected_classes: Names of classes used for naming for new images
-        :type detected_classes: list
-        :param input_folder: Input folder for images to recognise classes
-        :type input_folder: str
-        :param output_folder: Output folder for saving cropped images
-        :type output_folder: str
+    Parameters:
+    :param file: Input img file on which cropping is performed
+    :type file: str
+    :param results: Detected bounding boxes
+    :type results: list
+    :param detected_classes: Names of classes used for naming for new images
+    :type detected_classes: list
+    :param input_folder: Input folder for images to recognise classes
+    :type input_folder: str
+    :param output_folder: Output folder for saving cropped images
+    :type output_folder: str
     """
     # Load the original image
     image = input_folder + file
@@ -57,7 +79,7 @@ def crop_image(file: str, results: list, detected_classes: list, input_folder: s
     for i, box in enumerate(boxes):
         x1, y1, x2, y2 = box
         # Crop the object using the bounding box coordinates
-        ultralytics_crop_object = img[int(y1): int(y2), int(x1): int(x2)]
+        ultralytics_crop_object = img[int(y1) : int(y2), int(x1) : int(x2)]
         # Save the cropped object as an image
         file_name = Path(file).stem
         try:
@@ -71,15 +93,15 @@ def crop_image(file: str, results: list, detected_classes: list, input_folder: s
 
 def get_cropped_images_names(model: YOLO, results: list) -> list:
     """
-        Creates a list of names for naming new files.
+    Creates a list of names for naming new files.
 
-        Parameters:
-        :param model: YOLO model used for object detection
-        :type model: YOLO
-        :param results: Detected bounding boxes
-        :type results: list
-        :return: A list of images classes names detected by YOLO
-        :rtype: List[str]
+    Parameters:
+    :param model: YOLO model used for object detection
+    :type model: YOLO
+    :param results: Detected bounding boxes
+    :type results: list
+    :return: A list of images classes names detected by YOLO
+    :rtype: List[str]
     """
     names = model.names
     detected_classes = []
@@ -93,19 +115,23 @@ def get_cropped_images_names(model: YOLO, results: list) -> list:
 
 def numbering_classes_names(detected_classes: list) -> list:
     """
-        Crops and saves an images with regard to detected bounding boxes.
+    Crops and saves an images with regard to detected bounding boxes.
 
-        Parameters:
-        :param detected_classes: A list of images classes names detected by YOLO
-        :type detected_classes: List[str]
-        :return: A list of images classes names detected by YOLO with added numbering
-        :rtype: List[str]
+    Parameters:
+    :param detected_classes: A list of images classes names detected by YOLO
+    :type detected_classes: List[str]
+    :return: A list of images classes names detected by YOLO with added numbering
+    :rtype: List[str]
     """
     duplicates = [
-        item for item, count in collections.Counter(detected_classes).items() if count > 1
+        item
+        for item, count in collections.Counter(detected_classes).items()
+        if count > 1
     ]
     unique = [
-        item for item, count in collections.Counter(detected_classes).items() if count == 1
+        item
+        for item, count in collections.Counter(detected_classes).items()
+        if count == 1
     ]
 
     # Changing unique names in a list
@@ -122,3 +148,43 @@ def numbering_classes_names(detected_classes: list) -> list:
                 duplicate_number += 1
 
     return detected_classes
+
+
+def convert_pdf_to_images(pdf_name: str, first_page: int, last_page: int):
+    """
+    Converts a document in a pdf format to multiple png images corresponding to 2_pages
+
+    Parameters:
+    :param pdf_name: Name to the PDF that you want to convert
+    :type pdf_name: str
+    :param first_page: First page of a PDF file from where you want to start converting.
+    :type first_page: int
+    :param last_page: Last page of a PDF file to where you want to start converting.
+    :type last_page: int
+    """
+    pages = convert_from_path(
+        NEWSPAPERS_DIR + pdf_name,
+        poppler_path=POPPLER_PATH,
+        first_page=first_page,
+        last_page=last_page,
+    )
+    pdf_name = Path(NEWSPAPERS_DIR + pdf_name).stem
+    for i in range(len(pages)):
+        pages[i].save(f"{PAGES_DIR}{pdf_name}_page_{str(i)}.png", "PNG")
+
+
+def clear_folders():
+    """
+    Clearing folders from files before every detection and cropping
+    """
+    folders = [PAGES_DIR, ARTICLES_DIR, ARTICLES_CROPPED_DIR, RESULTS_DIR]
+    for folder in folders:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print("Failed to delete %s. Reason: %s" % (file_path, e))
--- scr/image_cropping/main.py
+++ scr/image_cropping/main.py
@@ -1,13 +1,18 @@
-from definitions import PAGES_DIR, ARTICLES_DIR, ARTICLES_CROPPED_DIR
-from scr.helpers.functions import convert_pdf_to_images
-from scr.image_cropping.functions import detect_and_crop_images
+from definitions import ARTICLES_CROPPED_DIR, ARTICLES_DIR, PAGES_DIR
+from scr.image_cropping.functions import (clear_folders, convert_pdf_to_images,
+                                          detect_and_crop_images)
 
 if __name__ == "__main__":
     print("START")
-    convert_pdf_to_images(pdf_name="1.pdf", first_page=0, last_page=2)
-    detect_and_crop_images(model_name="newspaper_best.pt", input_folder=PAGES_DIR, output_folder=ARTICLES_DIR)
-    detect_and_crop_images(model_name="article_best.pt", input_folder=ARTICLES_DIR,
-                           output_folder=ARTICLES_CROPPED_DIR)
-
-    # TODO
-    # 2) deleting old files before nex detection
+    clear_folders()
+    convert_pdf_to_images(pdf_name="1.pdf", first_page=0, last_page=1)
+    detect_and_crop_images(
+        model_name="newspaper_best.pt",
+        input_folder=PAGES_DIR,
+        output_folder=ARTICLES_DIR,
+    )
+    detect_and_crop_images(
+        model_name="article_best.pt",
+        input_folder=ARTICLES_DIR,
+        output_folder=ARTICLES_CROPPED_DIR,
+    )
