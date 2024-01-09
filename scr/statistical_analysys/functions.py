22067a0b45c2efa422b34a1a9eb3ebf68a1d94fa
Sandpitturtleee <karolmazur@onet.eu>
2024-01-09 18:34:32 +0100

    Adding reading areas from predefined labels, on corrected sized images with reading its size

--- scr/statistical_analysys/functions.py
+++ scr/statistical_analysys/functions.py
@@ -1,16 +1,31 @@
 import os
 import re
+from ultralytics import YOLO
+import cv2
 
-from definitions import PAGES_TRAIN_LABELS_DIR
-from natsort import natsorted
+
+def detect_images(model_name: str, input_path: str):
+    model = YOLO(model_name)
+    for file in sorted_alphanumeric(os.listdir(input_path)):
+        results = model(input_path + file, device="mps")
+        # print(file)
+        # print(results[0].boxes.xyxy.tolist())
 
 
-def load_files_and_calculate_areas(path: str):
+def load_files_and_calculate_areas(txt_input_path: str, img_input_path):
     areas = []
-    for file in sorted_alphanumeric(os.listdir(path)):
+    iter = 0
+    img_sizes = get_img_sizes(img_input_path=img_input_path)
+    for file in sorted_alphanumeric(os.listdir(txt_input_path)):
         bounding_boxes = read_bounding_boxes(
-            path=path + file)
-        areas.append(calculate_area(img_width=640, img_length=640, bounding_boxes=bounding_boxes))
+            path=txt_input_path + file)
+        # print(file)
+        # print(bounding_boxes)
+        # print(calculate_area(img_width=640, img_length=640, bounding_boxes=bounding_boxes))
+        # print()
+        areas.append(calculate_area(img_width=img_sizes[iter][0], img_length=img_sizes[iter][1], bounding_boxes=bounding_boxes))
+        iter += 1
+
     return areas
 
 
@@ -28,6 +43,14 @@ def calculate_area(img_width: int, img_length: int, bounding_boxes: list):
         areas.append(float(item[3]) * img_width * float(item[4]) * img_length)
     return areas
 
+
+def get_img_sizes(img_input_path: str):
+    img_sizes = []
+    for file in sorted_alphanumeric(os.listdir(img_input_path)):
+        im = cv2.imread(img_input_path + file)
+        h, w, _ = im.shape
+        img_sizes.append([w, h])
+    return img_sizes
 
 def to_matrix(list_to_convert: list, split_length: int):
     return [list_to_convert[i: i + split_length] for i in range(0, len(list_to_convert), split_length)]
--- scr/statistical_analysys/main.py
+++ scr/statistical_analysys/main.py
@@ -1,7 +1,9 @@
-from definitions import PAGES_TRAIN_LABELS_DIR, ARTICLES_TRAIN_LABELS_DIR
-from scr.statistical_analysys.functions import load_files_and_calculate_areas
+from definitions import PAGES_ANALYZE_IMAGES_DIR, PAGES_ANALYZE_LABELS_DIR
+from scr.statistical_analysys.functions import load_files_and_calculate_areas, detect_images
 
 if __name__ == "__main__":
     print("START")
-    areas = load_files_and_calculate_areas(path=ARTICLES_TRAIN_LABELS_DIR)
+    areas = load_files_and_calculate_areas(txt_input_path=PAGES_ANALYZE_LABELS_DIR,img_input_path=PAGES_ANALYZE_IMAGES_DIR)
     print(areas)
+
+    #detect_images(model_name="newspaper_best.pt",input_path=PAGES_ANALYZE_IMAGES_DIR)
