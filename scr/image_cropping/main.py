1bbd312a213d873dd8da30b1a263d67deeea6e9b
Sandpitturtleee <karolmazur@onet.eu>
2024-02-26 22:02:36 +0100

    Adding documentation for text_analysys.py

--- scr/image_cropping/main.py
+++ scr/image_cropping/main.py
@@ -1,17 +1,5 @@
-import copy
-from pprint import pprint
-
-import numpy as np
-from matplotlib import pyplot as plt
-
-from definitions import ARTICLES_CROPPED_DIR, ARTICLES_DIR, PAGES_DIR
-from scr.image_cropping.coordinate_sorting import create_cols, list_min_val
 from scr.image_cropping.cropping import (
-    clear_folders,
-    convert_pdf_to_images,
     cropping,
-    detect_and_crop_images_articles,
-    detect_and_crop_images_pages,
 )
 
 if __name__ == "__main__":
--- scr/statistical_analysys/text_analysys.py
+++ scr/statistical_analysys/text_analysys.py
@@ -1,34 +1,32 @@
-import os
 import re
-import shutil
 from os import listdir
 from os.path import isfile, join
-from pprint import pprint
-
-from scipy.spatial.distance import hamming
+from typing import Any
 
 from definitions import DETECTED_TEXT, EXPECTED_TEXT
 from scr.statistical_analysys.plot_functions import create_bar_plot
 
 
 def analyze_text():
+    """
+        Analyzes text matching percentage and creates plots for them
+    """
     article_percentages, percentages_list = analyze_percentages()
     print(article_percentages)
     for item in percentages_list:
         create_bar_plot(data=item, bin_edges=area_bin_edges, bin_labels=area_bin_labels)
 
 
-def analyze_percentages():
+def analyze_percentages() -> tuple[list, list]:
+    """
+        Read text from txt files and create a list of percentages
+
+        Parameters:
+        :return: A tuple of list of percentages
+        :rtype: tuple[list,list]
+    """
     text_expected = read_files_detected(path=EXPECTED_TEXT)
     text_detected = read_files_detected(path=DETECTED_TEXT)
-
-    # text_expected =["Doniesienia o kibolach bijących piłkarzy Legii, którzy ośmielili się przegrać z Lechem, szokują tylko niezorientowanych w realiach tzw. ekstraklasy."]
-    # text_detected =["oniesienia 0 kibolach D bijących piłkarzy Lcgii; którzy ośmielili SIę przegrać z Lechem; szokują tyl ko niczoricntowanych w rcaliach tzw. ekstraklasy."]
-    # text_expected = ["Doniesienia o kibolach bijących piłkarzy Legii, którzy ośmielili się przegrać"]
-    # text_detected = ["oniesienia 0 0 0 0 0 0 0 kibolach D bijących piłkarzy Lcgii; którzy ośmielili SIę przegrać"]
-    # text_expected = ["Dziś pierwsze urodziny tego symbolu kobiecej siły i siostrzeństwa ponad podziałami - ubiegłego 3 października powstrzymałyśmy rząd przed wprowadzeniem całkowitego zakazu aborcji."]
-    # text_detected = ["ziśpierwszeurodzinyte D go symbolukobicccj siły isiostrzenstwa ponadpodziałami -ubiegłego 3 października powstrzymałyśmy rządprzcd wprowadzeniem całkowitego zakazu aborcji."]
-
     converted_detected = create_list_of_words(text=text_detected)
     converted_expected = create_list_of_words(text=text_expected)
 
@@ -41,7 +39,23 @@ def analyze_percentages():
     return article_percentages, percentages_list
 
 
-def word_matching(text_expected, text_detected, word_threshold, search_depth):
+def word_matching(text_expected: list[list[str]], text_detected: list[list[str]], word_threshold: float,
+                  search_depth: int) -> tuple[list, list]:
+    """
+        Creates tuple of lists of percentages for all txt files
+
+        Parameters:
+        :param text_expected: Baseline text to compare to
+        :type text_expected: list[list[str]]
+        :param text_detected: Detected text
+        :type text_detected: list[list[str]]
+        :param word_threshold: Percentage threshold where a word is getting classified as recognised correctly
+        :type word_threshold: float
+        :param search_depth: Amount of steps made in algorythm in search of an appearance of a word in detected text
+        :type search_depth: int
+        :return: A tuple of list of percentages
+        :rtype: tuple[list,list]
+    """
     article_percentages = []
     percentages_list = []
     for e, d in zip(text_expected, text_detected):
@@ -50,7 +64,7 @@ def word_matching(text_expected, text_detected, word_threshold, search_depth):
             detected=d,
             word_threshold=word_threshold,
             search_depth=search_depth,
-            min_word_lenght=4,
+            min_word_length=4,
         )
         article_percentages.append(article_percentage)
         percentages_list.append(percentages)
@@ -58,8 +72,26 @@ def word_matching(text_expected, text_detected, word_threshold, search_depth):
 
 
 def word_matching_article(
-    expected, detected, word_threshold, search_depth, min_word_lenght
-):
+        expected: list[list[str]], detected: list[list[str]], word_threshold: float, search_depth: int,
+        min_word_length: int
+) -> tuple[float, list[float | Any]]:
+    """
+        Creates tuple of lists of percentages for txt file in a single article
+
+        Parameters:
+        :param expected: Baseline text to compare to
+        :type expected: list[list[str]]
+        :param detected: Detected text
+        :type detected: list[list[str]]
+        :param word_threshold: Percentage threshold where a word is getting classified as recognised correctly
+        :type word_threshold: float
+        :param search_depth: Amount of steps made in algorythm in search of an appearance of a word in detected text
+        :type search_depth: int
+        :param min_word_length: Minimal length of a word to be accounted in statistics
+        :type min_word_length: int
+        :return: A tuple of list of percentages
+        :rtype: tuple[float, list[float | Any]]
+    """
     not_detected_streak, matching_words, last_word_index = 0, 0, 0
     search_depth_original = search_depth
     percentages = []
@@ -89,12 +121,23 @@ def word_matching_article(
             pass
         percentages.append(max(percentages_nested))
     percentage = matching_words / get_number_of_long_words(
-        list_name=expected, length=min_word_lenght
+        list_name=expected, length=min_word_length
     )
     return percentage, percentages
 
 
-def match_word_percentage(e, d):
+def match_word_percentage(e: list[str], d: list[str]) -> float:
+    """
+        Calculates % of words matching parts
+
+        Parameters:
+        :param e: Baseline text to compare to
+        :type e: list[str]
+        :param d: Detected text
+        :type d: list[str]
+        :return: Percentage as a float ex. 0.5
+        :rtype: float
+    """
     e, d = add_string_parts(e=e, d=d)
     x = create_occurrences(word=e)
     y = create_occurrences(word=d)
@@ -103,11 +146,20 @@ def match_word_percentage(e, d):
     return sum_dict / len(e)
 
 
-def add_string_parts(e, d):
+def add_string_parts(e: list[str], d: list[str]) -> tuple[list, list]:
+    """
+        Add # to make two string matching in length for further processing
+
+        Parameters:
+        :param e: Baseline text to compare to
+        :type e: list[str]
+        :param d: Detected text
+        :type d: list[str]
+        :return: New strings with matching length
+        :rtype: float
+    """
     e_length = len(e)
     d_length = len(d)
-    # print(f"Expected sp {e}")
-    # print(f"Detected sp {d}")
     length_diff = abs(e_length - d_length)
     string_diff = "#" * length_diff
     if e_length < d_length:
@@ -117,23 +169,32 @@ def add_string_parts(e, d):
     return e, d
 
 
-def create_list_of_words(text):
+def create_list_of_words(text: list[str]) -> list[list[str]]:
+    """
+        Split text to single words
+
+        Parameters:
+        :param text: Text
+        :type text: list[str]
+        :return: Text split by " " into words
+        :rtype: list[list[str]]
+    """
     result = []
     for item in text:
         result.append(list(item.split(" ")))
     return result
-    # expected_result = list(expected.split(" "))
-    # return expected_result
 
 
-def compare_length(detected, expected):
-    text_length = []
-    for d, e in zip(detected, expected):
-        text_length.append(f"{len(e)},{len(d)}")
-    return text_length
+def read_files_detected(path: str) -> list[str]:
+    """
+        Read multiple txt files to list
 
-
-def read_files_detected(path):
+        Parameters:
+        :param path: Path to txt file
+        :type path: str
+        :return: Text read from multiple txt files
+        :rtype: list[str]
+    """
     file_names_detected = get_file_names(folder_path=path)
     file_paths_detected = create_file_paths(
         file_names=file_names_detected, folder_path=path
@@ -144,13 +205,32 @@ def read_files_detected(path):
     return text
 
 
-def read_txt_to_str(path):
+def read_txt_to_str(path: str) -> str:
+    """
+        Read single txt file
+
+        Parameters:
+        :param path: Path to txt file
+        :type path: str
+        :return: Text read from txt file
+        :rtype: str
+    """
     with open(path, "r") as file:
         data = file.read().rstrip()
     return data
 
 
-def get_file_names(folder_path: str):
+def get_file_names(folder_path: str) -> list:
+    """
+        Read single txt file
+
+        Parameters:
+        :param folder_path: Path to folder
+        :type folder_path: str
+        :return: A list of files
+        :rtype: list
+    """
+    only_files = []
     try:
         only_files = [
             f
@@ -159,24 +239,40 @@ def get_file_names(folder_path: str):
         ]
         only_files.remove(".DS_Store")
     except ValueError:
-        print(".Ds_Store not found")
+        pass
     return only_files
 
 
-def sorted_alphanumeric(data):
-    convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
-    return sorted(data, key=alphanum_key)
+def create_file_paths(file_names: list, folder_path: str)->list:
+    """
+        Creates path for files
 
-
-def create_file_paths(file_names, folder_path):
+        Parameters:
+        :param file_names: Names of files in a folder
+        :type file_names: list
+        :param folder_path: Path to folder
+        :type folder_path: str
+        :return: A list of file paths
+        :rtype: list
+    """
     file_paths = []
     for item in file_names:
         file_paths.append(f"{folder_path + item}")
     return file_paths
 
 
-def get_number_of_long_words(list_name, length):
+def get_number_of_long_words(list_name: list, length: int)->int:
+    """
+        Creates path for files
+
+        Parameters:
+        :param list_name: List with words
+        :type list_name: list
+        :param length: Length of a word
+        :type length: int
+        :return: Amount of words of desired length
+        :rtype: int
+    """
     count = 0
     for item in list_name:
         if len(item) >= length:
@@ -184,13 +280,28 @@ def get_number_of_long_words(list_name, length):
     return count
 
 
-def create_occurrences(word):
+def create_occurrences(word: list) ->dict:
+    """
+        Creates dictionary of letters in a word with its occurrences
+
+        Parameters:
+        :param word: A word
+        :type word: list
+        :return: Dictionary
+        :rtype: dict
+    """
     unique = list(set(word))
     occurrences = []
     for item in unique:
         occurrences.append(word.count(item))
     dictionary = dict(zip(unique, occurrences))
     return dictionary
+
+
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
+    return sorted(data, key=alphanum_key)
 
 
 area_bin_edges = [
