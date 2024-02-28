2b5dcd18b16e55a9f03a1c7c98edb80fac3959c8
Sandpitturtleee <karolmazur@onet.eu>
2024-02-28 17:45:16 +0100

    Final fixes

--- scr/statistical_analysys/main.py
+++ scr/statistical_analysys/main.py
@@ -3,14 +3,11 @@ from scr.statistical_analysys.analyze_detect_functions import (
     analyze_pages,
 )
 from scr.statistical_analysys.text_analysys import (
-    add_string_parts,
     analyze_text,
-    create_occurrences,
-    match_word_percentage,
 )
 
 if __name__ == "__main__":
     print("START")
-    # analyze_pages()
-    # analyze_articles()
+    analyze_pages()
+    analyze_articles()
     analyze_text()
